54b36dffa118e509f1ab88b55b757dce9a6595c8
FinityM <B00125089@mytudublin.ie>
2022-03-01 14:14:57 +0000

    Properly completed the Registry class

--- Lab4/src/Registry.java
+++ Lab4/src/Registry.java
@@ -14,4 +14,19 @@ public class Registry {
     public void print(String message){
         System.out.println(message);
     }
+
+    public static Object singletonObjects(String s){
+        if (s == "Message Manager"){
+            return MessageManager.getManager();
+        } else if (s == "Log Manager"){
+            return LogManager.getLogManager();
+        } else if (s == "Window Manager"){
+            return WindowManager.getManager();
+        } else if (s == "Print Manager"){
+            return PrintManager.getPrintManager();
+        } else {
+            return null;
+        }
+
+    }
 }
--- Lab4/src/RegistryTest.java
+++ Lab4/src/RegistryTest.java
@@ -1,10 +1,25 @@
 public class RegistryTest {
     public static void main(String[] args) {
-    Registry r = Registry.getRegistry();
+    /* Singleton object only ever has a single address type
+    *  Object can be changed with a method within the registry
+    * */
 
-    r.print("New Register");
+    LogManager lm = (LogManager) Registry.singletonObjects("Log Manager");
 
-    System.out.println("r = " + r);
+    System.out.println("lm = " + lm);
+
+    PrintManager pm = (PrintManager) Registry.singletonObjects("Print Manager");
+
+    System.out.println("pm = " + pm);
+
+    WindowManager wm = (WindowManager) Registry.singletonObjects("Window Manager");
+
+    System.out.println("wm = " + wm);
+
+    MessageManager mm = (MessageManager) Registry.singletonObjects("Message Manager");
+
+    System.out.println("mm = " + mm);
+
 
     }
 }
